5eacb4d31de2eff61b35e3a9232b027ec96c27b8
Himanshu Kumar <himanshu151281@gmail.com>
2024-09-22 18:27:57 +0530

    event section fixed

--- src/components/home/Events.jsx
+++ src/components/home/Events.jsx
@@ -4,118 +4,120 @@ import styles from "../../styles/home/event.module.scss";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
 import { SectionWrapper } from "@/hoc";
-import { motion, useScroll, useTransform } from "framer-motion";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  AnimatePresence,
+} from "framer-motion";
 import { fadeIn, slideIn, textVariant, zoomIn } from "@/utils/motion";
 import { useMyContext } from "@/context";
 
 const Events = () => {
-  const [mode, setMode] = useState("");
   const { eventsData } = useMyContext();
-
   const [arr, setArr] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    setMode("online");
-  }, []);
+    setArr(eventsData);
+  }, [eventsData]);
 
-  useEffect(() => {
-    if (mode === "online") {
-      setArr(eventsData.filter((event) => event.type === "online"));
-    } else {
-      setArr(eventsData.filter((event) => event.type === "offline"));
-    }
-  }, [mode]);
-
-  // console.log({ arr });
-
-  const EventsContainer = useCallback(
-    ({ event, idx }) => {
-      return (
-        <div
-          key={mode + idx}
-          variants={fadeIn("right", "spring", 1 * 0.8, 1.25)}
-          className={styles.eventContainer}
-        >
-          <h2>{event.title}</h2>
-          {/* {console.log(event)} */}
-
-          <div className={styles.wrapper}>
-            <Marquee autoFill>
-              {event.images.map((img, jdx) => (
-                <Image
-                  key={jdx}
-                  src={img || "/images/founder-img.jpg"}
-                  alt={event.title}
-                  width={200}
-                  height={200}
-                  className={styles.img}
-                />
-              ))}
-            </Marquee>
-          </div>
-        </div>
-      );
-    },
-    [arr, mode]
-  );
+  // const EventsContainer = useCallback(
+  //   ({ event, idx }) => {
+  //     return (
+  //       <div
+  //         key={idx}
+  //         variants={fadeIn("right", "spring", 1 * 0.8, 1.25)}
+  //         className={styles.eventContainer}
+  //       >
+  //         <h2>{event.title}</h2>
+  //         <div className={styles.wrapper}>
+  //           <Marquee autoFill>
+  //             {event.images.map((img, jdx) => (
+  //               <Image
+  //                 key={jdx}
+  //                 src={"/images/founder-img.jpg"}
+  //                 alt={event.title}
+  //                 width={200}
+  //                 height={200}
+  //                 className={styles.img}
+  //               />
+  //             ))}
+  //           </Marquee>
+  //         </div>
+  //       </div>
+  //     );
+  //   },
+  //   [arr]
+  // );
 
   return (
     <div className={styles.eventSection}>
       <motion.h1 variants={textVariant()}>Events</motion.h1>
       <hr />
 
-      <motion.div
-        variants={fadeIn("up", "spring", 0.3, 1.25)}
-        className={styles.switch}
-      >
-        <div className={styles.wrapper}>
-          <span onClick={() => setMode("online")}>Online</span>
-          <span onClick={() => setMode("offline")}>Offline</span>
-          <span
-            className={
-              styles.overlay +
-              " " +
-              `${mode === "online" ? "translate-x-0" : "translate-x-full"}`
-            }
-          ></span>
-        </div>
-      </motion.div>
-
-      {[...arr].map((event, idx) => (
+      {/* {[...arr].map((event, idx) => (
         <EventsContainer key={idx} event={event} idx={idx} />
-      ))}
+      ))} */}
 
       <motion.div
         variants={fadeIn("right", "spring", 1 * 0.8, 1.25)}
         className={styles.eventContainer}
       >
         <h2>Acetrix War League</h2>
-
         <div className={styles.wrapper}>
           <Marquee>
             <Image
-              src={"/images/founder-img.jpg"}
+              src={"/images/team/founder/alu.jpg"}
               alt="founder"
               width={200}
               height={200}
               className={styles.img}
             />
             <Image
-              src={"/images/founder-img.jpg"}
+              src={"/images/team/founder/cre.jpg"}
               alt="founder"
               width={200}
               height={200}
               className={styles.img}
             />
             <Image
-              src={"/images/founder-img.jpg"}
+              src={"/images/team/founder/alu.jpg"}
               alt="founder"
               width={200}
               height={200}
               className={styles.img}
             />
             <Image
-              src={"/images/founder-img.jpg"}
+              src={"/images/team/founder/cre.jpg"}
+              alt="founder"
+              width={200}
+              height={200}
+              className={styles.img}
+            />
+            <Image
+              src={"/images/team/founder/alu.jpg"}
+              alt="founder"
+              width={200}
+              height={200}
+              className={styles.img}
+            />
+            <Image
+              src={"/images/team/founder/cre.jpg"}
+              alt="founder"
+              width={200}
+              height={200}
+              className={styles.img}
+            />
+            <Image
+              src={"/images/team/founder/alu.jpg"}
+              alt="founder"
+              width={200}
+              height={200}
+              className={styles.img}
+            />
+            <Image
+              src={"/images/team/founder/cre.jpg"}
               alt="founder"
               width={200}
               height={200}
@@ -126,13 +128,12 @@ const Events = () => {
       </motion.div>
 
       <motion.div
-        variants={fadeIn("right", "spring", 2 * 0.8, 1.25)}
+        variants={fadeIn("right", "spring", 1 * 0.8, 1.25)}
         className={styles.eventContainer}
       >
-        <h2>Valfire</h2>
-
+        <h2>Duke o death</h2>
         <div className={styles.wrapper}>
-          <Marquee>
+          <Marquee direction="right">
             <Image
               src={"/images/founder-img.jpg"}
               alt="founder"
@@ -156,6 +157,34 @@ const Events = () => {
             />
             <Image
               src={"/images/founder-img.jpg"}
+              alt="founder"
+              width={200}
+              height={200}
+              className={styles.img}
+            />
+            <Image
+              src={"/images/team/founder/alu.jpg"}
+              alt="founder"
+              width={200}
+              height={200}
+              className={styles.img}
+            />
+            <Image
+              src={"/images/team/founder/cre.jpg"}
+              alt="founder"
+              width={200}
+              height={200}
+              className={styles.img}
+            />
+            <Image
+              src={"/images/team/founder/alu.jpg"}
+              alt="founder"
+              width={200}
+              height={200}
+              className={styles.img}
+            />
+            <Image
+              src={"/images/team/founder/cre.jpg"}
               alt="founder"
               width={200}
               height={200}
@@ -163,11 +192,196 @@ const Events = () => {
             />
           </Marquee>
         </div>
-        {/*  */}
-        <div className={styles.galleryBtn}>
-          <CallToAction href={"#"}>Gallery</CallToAction>
-        </div>
       </motion.div>
+      {/* Previous Button */}
+      {/* <div className={styles.galleryBtn}>
+        <CallToAction>
+          {isVisible ? 'Show Less' : 'Show More'}
+        </CallToAction>
+      </div> */}
+      {/* New Button */}
+      <div
+        className={styles.galleryBtn}
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        <CallToAction>{isVisible ? "Show Less" : "Show More"}</CallToAction>
+      </div>
+
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            // initial={{ height: 0, opacity: 0 }}
+            // animate={{ height: "auto", opacity: 1 }}
+            // exit={{ height: 0, opacity: 0 }}
+            // transition={{ duration: 0.5 }}
+            className={styles.eventContainer}
+          >
+            <h2>Chess Tournament</h2>
+            <div className={styles.wrapper}>
+              <Marquee>
+                <Image
+                  src={"/images/founder-img.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/founder-img.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/founder-img.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/founder-img.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/alu.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/cre.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/alu.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/cre.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/alu.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/cre.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/alu.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/cre.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+              </Marquee>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            // initial={{ height: 0, opacity: 0 }}
+            // animate={{ height: 'auto', opacity: 1 }}
+            // exit={{ height: 0, opacity: 0 }}
+            // transition={{ duration: 0.5 }}
+            className={styles.eventContainer}
+          >
+            <h2>Valfire</h2>
+            <div className={styles.wrapper}>
+              <Marquee direction="right">
+                <Image
+                  src={"/images/founder-img.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/founder-img.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/founder-img.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/founder-img.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/alu.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/cre.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/alu.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+                <Image
+                  src={"/images/team/founder/cre.jpg"}
+                  alt="founder"
+                  width={200}
+                  height={200}
+                  className={styles.img}
+                />
+              </Marquee>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
